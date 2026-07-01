3cbc238cae05a97cb3f5b4fde1d52322b47f3d77
barakbenishay1 <barakbenishay1@gmail.com>
2026-07-01 20:23:56 +0300

    database

--- src/com/fitwell/boundary/consultant/ConsultantDashboardUI.java
+++ src/com/fitwell/boundary/consultant/ConsultantDashboardUI.java
@@ -25,10 +25,10 @@ import javax.swing.SwingConstants;
 import javax.swing.Timer;
 import javax.swing.border.LineBorder;
 
-import com.fitwell.boundary.InventoryReportForm;
-import com.fitwell.boundary.ReportParametersForm;
 import com.fitwell.boundary.UIBuilder;
 import com.fitwell.boundary.login.RoleSelectionUI;
+import com.fitwell.boundary.reports.InventoryReportForm;
+import com.fitwell.boundary.reports.ReportParametersForm;
 import com.fitwell.control.AuthenticationController;
 import com.fitwell.control.TrainingClassController;
 
